5e98a51cb62b505f283d3b95e82c838aeb1a13ca
Alan <allanz.ez@gmail.com>
2022-11-10 12:45:37 -0700

    first commit

--- fisioterapias/models.py
+++ fisioterapias/models.py
@@ -3,44 +3,42 @@ from django.db import models
 
 # Create your models here.
 
+class PE(models.Model):
+    programa_educativo = models.CharField(max_length=200, blank=False)
+    siglas = models.CharField(max_length=200, blank=False)
+
 class Procedencia(models.Model):
-    procedencia = models.CharField(max_length=200)
+    procedencia = models.CharField(max_length=200, blank=False)
 
     def __str__(self):
         return self.procedencia
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=200)
-    procedencia = models.ForeignKey(Procedencia, on_delete=models.CASCADE)
-    expediente = models.CharField(max_length=200)
-    PE = models.ForeignKey(PE, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.nombre + " "+self.expediente + " "+self.PE 
+    nombre = models.CharField(max_length=200, blank=False)
+    procedencia = models.ForeignKey(Procedencia, on_delete=models.CASCADE, blank=False)
+    expediente = models.CharField(max_length=200, blank=False)
+    PE = models.ForeignKey(PE, on_delete=models.CASCADE, blank=True)
 
 class Aula(models.Model):
-    aula = models.CharField(max_length=200)
+    aula = models.CharField(max_length=200, blank=False)
     
     def __str__(self):
         return self.aula
 
 class Cita(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    aula = models.ForeignKey(Aula, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, blank=False)
+    aula = models.ForeignKey(Aula, on_delete=models.CASCADE, blank=False)
     motivo = models.CharField(max_length=200, blank=False)
-    fecha = models.DateTimeField()
+    fecha = models.DateTimeField(blank=False)
+    hora_cierre = models.TimeField(blank=False)
     cumplio = models.BooleanField(default='0')
-    costo = models.FloatField()
+    costo = models.FloatField(blank=True)
 
     def __str__(self):
         return self.cliente.nombre
 
 class CierreCaja(models.Model):
-    cajero = models.CharField(max_length=200, blank=False)
-    fecha = models.DateTimeField()
-    cantidad = models.FloatField()
-    comentarios = models.CharField(max_length=200, blank=False)
-
-class PE(models.Model):
-    programa_educativo = models.CharField(max_length=200, blank=False)
-    siglas = models.CharField(max_length=200, blank=False)
+    cajero = models.CharField(max_length=200)
+    fecha = models.DateTimeField(blank=False)
+    cantidad = models.FloatField(blank=False)
+    comentarios = models.CharField(max_length=200, blank=True)    
