a0449c50a3dd80a8dffc688d57572c0d9b22dd94
Alan <allanz.ez@gmail.com>
2022-11-13 22:39:11 -0700

    Django app initial commit

--- UES/settings.py
+++ UES/settings.py
@@ -1,3 +1,5 @@
+import os
+from django.core.management.utils import get_random_secret_key
 """
 Django settings for UES project.
 
@@ -20,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_k&t9n*v9(=j9pba9u*i%$+gth%l_$rc=#vm1&ejrzz66_0^n0'
+#SECRET_KEY = 'django-insecure-_k&t9n*v9(=j9pba9u*i%$+gth%l_$rc=#vm1&ejrzz66_0^n0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -127,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
